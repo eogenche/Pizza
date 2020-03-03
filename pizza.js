e97587b63e6103056641b88fc666196381380ff0
monte <eogenche@yahoo.com>
2020-03-03 10:57:07 +0300

    DELIVERY function committed

--- pizza.js
+++ pizza.js
@@ -29,30 +29,60 @@ var pizza_topping={
     "olives":65,
     "sausages":70
 }
-
+var total, finalTotal;
 $(document).ready(function() {
     $("#button").click(function(event) {
-      event.preventDefault();
-      var psize = parseInt($("#list").val());
-      var pcrust = parseInt($("#list1").val());
-      var topping = parseInt($("#list2").val());
-      var PizzaType = parseInt($("#list3").val());
-        var total = psize+pcrust+topping;
-      alert("YOU have ordered"+topping+" "+ pcrust+" "+ psize+" which  cost ${total} .");
-      var p_size = $("#list").val(); 
-      var p_crust = $("#list1").val();
-      var toppings = $("#list2").val(); 
-      var p_type = $("#list3").val(); 
-      
-      $("tbody").append("<tr>"+"<td>"+p_type+"</td>"+"<td>"+p_crust+"</td>"+"<td>" +toppings+ "</td>"+"<td>" + total +"</td>" +"<td>"+Ttopping+ "</td>" +"<td>" +price +"</td>"+"</tr>");
-      
+        event.preventDefault();
+        var psize = parseInt($("#list").val());
+        var pcrust = parseInt($("#list1").val());
+        var topping = parseInt($("#list2").val());
+        var PizzaType = parseInt($("#list3").val());
+        total = psize+pcrust+topping;
+        // alert(`total cost is ${total} .`);
 
-    //   Pizza_size.prototype.Total_Price = function() {
-        // return psize + pcrust+ topping;
-        var orderedPizza=new Pizza("psize","pcrust","topping");
-        debugger
-       ("#output").text(orderedPizza.fullPizza());
+        function confirmDelivery(){
+            var delivery = confirm("Do you want your Pizza delivered to you?");
+            if(delivery){
+                finalTotal=total+150;
+                var location = prompt("Please enter your location");
+                alert("Your pizza will be delivered to "+location + " at Ksh 150.Total cost is "+finalTotal);
+            }
+        }
+    // document.getElementById("output").innerHTML = "The total cost for your order is: "+total+" Thank you for shopping with us.";
+        confirmDelivery();
+
+
+      
+    //   $("tbody").append("<tr>"+"<td>"+p_type+"</td>"+"<td>"+p_crust+"</td>"+"<td>" +Topping+ "</td>"+"<td>" + total +"</td>" +"<td>"+Ttopping+ "</td>" +"<td>" +price +"</td>"+"</tr>");
+
+    // //   Pizza_size.prototype.Total_Price = function() {
+    //     // return psize + pcrust+ topping;
+    //     var orderedPizza=new Pizza("psize","pcrust","topping");
+    //     debugger
+    //    ("#output").text(orderedPizza.fullPizza());
     });
+    function confirmDelivery(){
+        var delivery = confirm("Do you want your Pizza delivered to you?");
+        if(delivery){
+            total=total+150;
+            var location = prompt("Please enter your location");
+            alert("Your pizza will be delivered to "+location + " at Ksh 150." );
+        }
+        document.getElementById("output").innerHTML = "The total cost for your order is: "+total+" Thank you for shopping with us.";
+    }
+    function appendTotal(){
+        confirmDelivery();
+        $(".items > tbody:last").last().append('<tr>' + 
+        '<td>'+ 'Your total is:'+
+        '</td>'+
+        '<td>'+
+        total+
+        '</td>'+
+        '</tr>')
+        $("#checkout").attr("disabled", true);
+        $("#large").attr("disabled", true);
+        
+    }
     
 });
 
